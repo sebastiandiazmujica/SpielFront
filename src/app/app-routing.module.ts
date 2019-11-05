b2ec6b8eaee3a5c5e86827d05ea6622e57b1b913
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2019-11-05 10:22:07 -0500

    Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,6 @@ const routes: Routes = [
   { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'evento', loadChildren: './evento/evento.module#EventoPageModule' },
-  { path: 'partidos', loadChildren: './partidos/partidos.module#PartidosPageModule' },
   { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
 ];
 
